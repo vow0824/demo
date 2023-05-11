6adc288bfe52e37b67622449e9f45a72af4b29dd
vow <vow@qq.com>
2023-05-11 17:00:17 +0800

    添加配置

--- order.sql
+++ order.sql
@@ -1,4 +1,22 @@
-create table `order`(
-                        order_id bigint(16) primary key auto_increment,
-                        add_time datetime not null ,
-                        upd_time datetime not null on update CURRENT_TIMESTAMP);
+create database test;
+create database test_0;
+create database test_1;
+
+use test_0;
+create table app_order_0(
+                            order_id bigint(16) primary key auto_increment,
+                            add_time datetime not null ,
+                            upd_time datetime not null on update CURRENT_TIMESTAMP);
+create table app_order_1(
+                            order_id bigint(16) primary key auto_increment,
+                            add_time datetime not null ,
+                            upd_time datetime not null on update CURRENT_TIMESTAMP);
+use test_1;
+create table app_order_0(
+                            order_id bigint(16) primary key auto_increment,
+                            add_time datetime not null ,
+                            upd_time datetime not null on update CURRENT_TIMESTAMP);
+create table app_order_1(
+                            order_id bigint(16) primary key auto_increment,
+                            add_time datetime not null ,
+                            upd_time datetime not null on update CURRENT_TIMESTAMP);
